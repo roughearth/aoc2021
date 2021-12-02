ad8a8625cfd606c70b44ccdf7d3e2f5ffcdebfcc
Richard Hubbard <richard.hubbard@armadry.co.uk>
2021-12-02 12:46:23 +0000

    day 2

--- src/days/day2/index.ts
+++ src/days/day2/index.ts
@@ -3,12 +3,61 @@ import {cleanAndParse} from '../../utils';
 
 export const meta = {};
 
+function parseLine(line: string) {
+  const [command, amount] = line.split(" ");
+  return {
+    command,
+    amount: Number(amount)
+  };
+}
+
 export function part1() {
-  const data = cleanAndParse(input, Number);
-  return `${data}`;
+  const data = cleanAndParse(input, parseLine);
+
+  let pos = 0;
+  let depth = 0;
+
+  data.forEach(({command, amount}) => {
+    switch (command) {
+      case 'up':
+        depth -= amount;
+        break;
+      case 'down':
+        depth += amount;
+        break;
+      case 'forward':
+        pos += amount;
+        break;
+      default:
+        console.log(command, "not found")
+    }
+  })
+
+  return pos * depth;
 }
 
 export function part2() {
-  const data = cleanAndParse(input, Number);
-  return `${data}`;
+  const data = cleanAndParse(input, parseLine);
+  let pos = 0;
+  let depth = 0;
+  let aim = 0;
+
+  data.forEach(({command, amount}) => {
+    switch (command) {
+      case 'up':
+        aim -= amount;
+        break;
+      case 'down':
+        aim += amount;
+        break;
+      case 'forward':
+        pos += amount;
+        depth += (aim * amount)
+        break;
+      default:
+        console.log(command, "not found")
+    }
+  })
+
+  return pos * depth;
 }
